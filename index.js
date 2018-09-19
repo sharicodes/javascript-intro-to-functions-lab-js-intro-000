eebb123f8e41615c612db3b03414d168b1ded510
srosenb <srosenb@yahoo.com>
2018-09-19 19:20:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ function logWhisper(newArg){
   console.log(newArg.toLowerCase());
 
 }
-function sayHiToGrandma(str1){
-  if(str1.toLowerCase === str1){
+
+
+function sayHiToGrandma(str1) {
+  if(str1.toLowerCase() === str1) {
     return "I can\'t hear you!";
-    } else if (str1.toUpperCase === str1){
+  } else if (str1.toUpperCase() === str1){
       return "YES INDEED!";
-    } else if(str1 === "I love you, Grandma."){
-
-
+    } else if(str1 ===  "I love you, Grandma."){
+      return "I love you, too."
   }
 }
 
